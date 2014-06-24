c3d5cfa9b97bd0e7ea41af5bb9f0307d2db67713
unknown <p1311821@IUTAINFOPC104.univ-lyon1.fr>
2014-06-24 10:32:00 +0200

    Premier commit de teste : -> add sql create scipt -> test index.html Bootstrap 3

---
+++ sql/create_table_and_sequence.sql
@@ -0,0 +1,12 @@
+drop table geek if exists cascade;
+drop sequence geek_seq if exists;
+
+create table geek (
+	id numeric not null,
+	name varchar(25) not null,
+	sex varchar(20) not null,
+	interests varchar(255) not null,
+	primary key (id)
+);
+
+create sequence geek_seq start with 1000;
